a914cc32ff973e6068307170fd5b4efe755061d9
Caio Santos <caiohenriquedassantos@gmail.com>
2023-06-04 22:58:33 -0300

    Cria estrutura da aplicação

---
+++ inc/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" href="img/icon/icon.ico" type="image/x-icon">
+
+  <?php include 'inc/inc-css.php'; ?>
+
+  <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+  <script src="https://kit.fontawesome.com/75eaba243c.js" crossorigin="anonymous"></script>
+
+  <title>Spotify Aplication | Caio</title>
+
+</head>
+
+<header>
+  <div class="header-section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <img src="" alt="" title="">
+        </div>
+        <div class="col-md-4">
+          <ul class="header-menu">
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+        <div class="col-md-4">
+
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
+
+</html>
---
+++ index.php
@@ -0,0 +1,17 @@
+<body>
+
+  <!-- Header -->
+  <?php include 'inc/header.php' ?>
+
+  <main>
+
+  </main>
+
+  <!-- Footer -->
+  <? include 'inc/footer.php' ?>
+
+  <!-- JS -->
+  <?php include 'inc/inc-js.php' ?>
+</body>
+
+</html>
